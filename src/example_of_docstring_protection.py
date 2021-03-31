96c868b6524c7891bd67e970bdeba173b4383029
hbrooks <seanhunterbrooks@gmail.com>
2021-03-31 16:37:35 -0400

    add base file for example of protection in docstring;

---
+++ src/example_of_docstring_protection.py
@@ -0,0 +1,10 @@
+import random
+
+
+def get_random_number():
+    """
+    Generates a random number between 0 and 9, inclusive.
+
+    < Document Guardian | Protect >
+    """
+    return random.randint(0, 10)
